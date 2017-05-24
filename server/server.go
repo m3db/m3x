a352ea52a5bbd3d063f597bdfc6b1cdd2046127c
Jerome Froelich <jeromefroe@uber.com>
2017-05-24 13:53:56 -0400

    Condense Serve method

--- server/server.go
+++ server/server.go
@@ -124,8 +124,7 @@ func (s *server) ListenAndServe() error {
 }
 
 func (s *server) Serve(l net.Listener) error {
-	addr := l.Addr()
-	s.address = addr.String()
+	s.address := l.Addr().String()
 	s.listener = l
 	go s.serve()
 	return nil

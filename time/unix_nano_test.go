90f69b3b4c4f61b45d57f1b129928f5af5732418
Richard Artoul <rartoul@uber.com>
2017-10-26 15:24:43 -0400

    Change assert to require

--- time/unix_nano_test.go
+++ time/unix_nano_test.go
@@ -24,7 +24,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -39,25 +38,25 @@ func TestUnixNanoBefore(t *testing.T) {
 	t0 := UnixNano(0)
 	t1 := UnixNano(1)
 
-	assert.Equal(t, true, t0.Before(t1))
-	assert.Equal(t, false, t1.Before(t0))
-	assert.Equal(t, false, t0.Before(t0))
+	require.Equal(t, true, t0.Before(t1))
+	require.Equal(t, false, t1.Before(t0))
+	require.Equal(t, false, t0.Before(t0))
 }
 
 func TestUnixNanoAfter(t *testing.T) {
 	t0 := UnixNano(0)
 	t1 := UnixNano(1)
 
-	assert.Equal(t, false, t0.After(t1))
-	assert.Equal(t, true, t1.After(t0))
-	assert.Equal(t, false, t0.After(t0))
+	require.Equal(t, false, t0.After(t1))
+	require.Equal(t, true, t1.After(t0))
+	require.Equal(t, false, t0.After(t0))
 }
 
 func TestUnixNanoEqual(t *testing.T) {
 	t0 := UnixNano(0)
 	t1 := UnixNano(1)
 
-	assert.Equal(t, false, t0.Equal(t1))
-	assert.Equal(t, false, t1.Equal(t0))
-	assert.Equal(t, true, t0.Equal(t0))
+	require.Equal(t, false, t0.Equal(t1))
+	require.Equal(t, false, t1.Equal(t0))
+	require.Equal(t, true, t0.Equal(t0))
 }

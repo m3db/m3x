f320f03d08df85ee25bf39721359943a734ce4e1
Richard Artoul <rartoul@uber.com>
2017-11-08 12:04:25 -0500

    Add comments

--- time/time_matcher_test.go
+++ time/time_matcher_test.go
@@ -12,11 +12,14 @@ func TestMatcher(t *testing.T) {
 	t1 := time.Now().UTC()
 	t2 := t1.In(loc)
 
+	// Make sure t1 and t2 don't == eachother
 	require.NotEqual(t, t1, t2)
 
+	// Make sure t1 and t2 Match eachother
 	t1Matcher := NewMatcher(t1)
 	require.True(t, t1Matcher.Matches(t2))
 
+	// Make sure the matcher doesn't always return true
 	require.NotEqual(t, t1Matcher, t2.Add(1*time.Hour))
 	require.NotEqual(t, t1Matcher, 10)
 }

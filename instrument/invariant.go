7c9a30767b58f3f950cf57baa3c629c8ca1795e4
Richard Artoul <rartoul@uber.com>
2018-10-23 16:15:42 -0400

    Add InvariantErrorf function

--- instrument/invariant.go
+++ instrument/invariant.go
@@ -21,6 +21,7 @@
 package instrument
 
 import (
+	"fmt"
 	"os"
 	"strings"
 
@@ -77,9 +78,26 @@ func EmitAndLogInvariantViolation(opts Options, f func(l log.Logger)) {
 	panicIfEnvSet()
 }
 
+// InvariantErrorf constructs a new error, prefixed with a string indicating that an invariant
+// violated occurred. Optionally panics if the ShouldPanicEnvironmentVariableName is set to "true".
+func InvariantErrorf(format string, a ...interface{}) error {
+	err := fmt.Errorf(format, a...)
+	panicIfEnvSetWithMessage(err.Error())
+	return err
+}
+
 func panicIfEnvSet() {
+	panicIfEnvSetWithMessage("")
+}
+
+func panicIfEnvSetWithMessage(s string) {
 	envIsSet := strings.ToLower(os.Getenv(ShouldPanicEnvironmentVariableName)) == "true"
+
 	if envIsSet {
-		panic("invariant violation detected")
+		if s == "" {
+			s = "invariant violation detected"
+		}
+
+		panic(s)
 	}
 }
